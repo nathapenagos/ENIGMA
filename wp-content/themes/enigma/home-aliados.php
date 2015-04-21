aa0461386ae415e15ce1f03f642d45c74d221254
nathapenagos <natha.penagos@gmial.com>
2015-04-21 00:42:39 -0500

    version 1.0

--- wp-content/themes/enigma/home-aliados.php
+++ wp-content/themes/enigma/home-aliados.php
@@ -1,44 +1,50 @@
 <div class="aliados">
  	<div class="row">
-   		<div class="col-lg-3">
+   <div class="enigma_heading_title">
+    <h3>NUESTROS ALIADOS<h3/>
+   </div>
+   	<div class="col-lg-2">
    		 <ul>
    		 	<li class="aliado1">
-            <a href="http://logopond.com/"> <img src="http://localhost:8888/wordpress/wp-content/uploads/2015/04/kids3.jpg"></a>   
+               <a href="http://logopond.com/"> <img src="http://localhost:8888/wordpress/wp-content/uploads/2015/04/logo1.png"></a>   
             </li>
-   		 	
    		 </ul>
 		</div>
-		<div class="col-lg-3">
-   		 <ul>
-   		 	<li>
-   		 		img
-   		 	</li>
-   		 	<li>
-   		 		link
-   		 	</li>
-   		 </ul>
-		</div>
-		<div class="col-lg-3">
-   		 <ul>
-   		 	<li>
-   		 		img
-   		 	</li>
-   		 	<li>
-   		 		link
-   		 	</li>
-   		 </ul>
-		</div>
-		<div class="col-lg-3">
-   		 <ul>
-   		 	<li>
-   		 		img
-   		 	</li>
-   		 	<li>
-   		 		link
-   		 	</li>
-   		 </ul>
-		</div>
+      <div class="col-lg-2">
+          <ul>
+            <li class="aliado1">
+               <a href="http://logopond.com/"> <img src="http://localhost:8888/wordpress/wp-content/uploads/2015/04/logo1.png"></a>   
+            </li>
+          </ul>
+      </div>
+      <div class="col-lg-2">
+          <ul>
+            <li class="aliado1">
+               <a href="http://logopond.com/"> <img src="http://localhost:8888/wordpress/wp-content/uploads/2015/04/logo1.png"></a>   
+            </li>
+          </ul>
+      </div>
+      <div class="col-lg-2">
+          <ul>
+            <li class="aliado1">
+               <a href="http://logopond.com/"> <img src="http://localhost:8888/wordpress/wp-content/uploads/2015/04/logo1.png"></a>   
+            </li>
+          </ul>
+      </div>
+         <div class="col-lg-2">
+          <ul>
+            <li class="aliado1">
+               <a href="http://logopond.com/"> <img src="http://localhost:8888/wordpress/wp-content/uploads/2015/04/logo1.png"></a>   
+            </li>
+          </ul>
+      </div>
+      <div class="col-lg-2">
+          <ul>
+            <li class="aliado1">
+               <a href="http://logopond.com/"> <img src="http://localhost:8888/wordpress/wp-content/uploads/2015/04/logo1.png"></a>   
+            </li>
+          </ul>
+      </div>
 
- 	
  	</div>
 </div>
